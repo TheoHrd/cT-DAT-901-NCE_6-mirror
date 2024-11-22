1c16cab5fe2d2407f3d68b64abccd0f98205aa0a
Jhana <mrgamagejhana@gmail.com>
2024-11-22 09:54:43 +0100

    Socket server

--- socket-api/main.go
+++ socket-api/main.go
@@ -2,53 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/gorilla/websocket"
+	"sync"
+	"socket-api/socket"
+	//"socket-api/druid"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true // Not secure
-	},
-}
 
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println("Erreur lors de l'upgrade WebSocket :", err)
-		return
-	}
-	defer conn.Close()
-
-	fmt.Println("Nouvelle connexion WebSocket établie")
-
-	for {
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println("Connexion fermée :", err)
-			break
-		}
-
-		fmt.Printf("Message reçu : %s\n", message)
-
-		
-		err = conn.WriteMessage(websocket.TextMessage, []byte("Message reçu : "+string(message)))
-		if err != nil {
-			fmt.Println("Erreur lors de l'envoi du message :", err)
-			break
-		}
-	}
-}
 
 func main() {
-	http.HandleFunc("/ws", handleWebSocket)
+	fmt.Printf("yo")
 
-	port := ":3006"
-	fmt.Println("Serveur WebSocket démarré sur le port", port)
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		fmt.Println("Erreur lors du démarrage du serveur :", err)
-	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		socket.StartServer(":3006")
+	}()
 }
---
+++ socket-api/socket/socket.go
@@ -0,0 +1,58 @@
+package socket
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Not secure
+	},
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Erreur lors de l'upgrade WebSocket :", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Nouvelle connexion WebSocket établie")
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Connexion fermée :", err)
+			break
+		}
+
+		fmt.Printf("Message reçu : %s\n", message)
+
+		
+		err = conn.WriteMessage(websocket.TextMessage, []byte("Message reçu : "+string(message)))
+		if err != nil {
+			fmt.Println("Erreur lors de l'envoi du message :", err)
+			break
+		}
+	}
+}
+
+func StartServer(port string) {
+
+	fmt.Printf("hello")
+
+
+
+	http.HandleFunc("/ws", handleWebSocket)
+
+	fmt.Println("Serveur WebSocket démarré sur le port", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Println("Erreur lors du démarrage du serveur :", err)
+	}
+}
